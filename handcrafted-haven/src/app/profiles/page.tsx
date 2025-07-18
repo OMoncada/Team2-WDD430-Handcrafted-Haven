65cb7889b3c8b2a7bc8b41c9dff6802b44e3912f
pearlrhema <pearlrhema8@gmail.com>
2025-07-18 21:14:04 +0100

    sellers, products listings, and ratings and reviews pages created.

---
+++ handcrafted-haven/src/app/profiles/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold mb-4">Profiles</h1>
+      <p className="text-lg">This is the Seller Profiles page.</p>
+    </div>
+  );
+}
